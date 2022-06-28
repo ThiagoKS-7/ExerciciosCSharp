c1a05fed6fb3442fd42301e0e3208781c660123b
Thiago Kasper de Souza <83460816+ThiagoKS-7@users.noreply.github.com>
2022-06-28 18:40:00 -0300

    Add files via upload

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var date = DateTime.Now;
+        Console.WriteLine($"Projeto 2 e 3 - Criando variáveis - {date}\n");
+        int idade = 27;
+        idade = (5 * 5) + 2;
+        int calculo = 27 - 5;
+        double peso = 5.5;
+        double calculoDouble = 7 / 5.0;
+        Console.WriteLine("Idade: " + idade);
+        Console.WriteLine("Calculo: " + calculo);
+        Console.WriteLine("float: " + peso);
+        Console.WriteLine("Calculo double: " + calculoDouble);
+        Console.WriteLine("\nPressione enter para continuar...");
+        Console.ReadLine();
+    } 
+}
---
+++ 3-CastsDeVariaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var date = DateTime.Now;
+        double salario = 3000.15;
+        int salInteiro = (int) salario;
+        long x = 300000000000000;
+        short y = 300;
+        float teste = 23.5454544F; // se usa pra ter mais precisão decimal (tem q ter F no fim)
+        Console.WriteLine($"Projeto 4 - Casts de variáveis - {date}\n");
+        Console.WriteLine(
+            $"salario: {salario}\n" +
+            $"salInteiro: {salInteiro}\n" +
+            $"long: {x}\n" +
+            $"short: {y}\n" +
+            $"teste: {teste}");
+        Console.WriteLine("\nPressione enter para continuar...");
+        Console.ReadLine();
+    }
+}
