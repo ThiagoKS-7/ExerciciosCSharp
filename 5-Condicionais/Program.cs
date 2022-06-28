8b671bad326e576a1156cd6c82de5da15b9d2742
Thiago Kasper de Souza <83460816+ThiagoKS-7@users.noreply.github.com>
2022-06-28 18:36:51 -0300

    Add files via upload

---
+++ 5-Condicionais/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var date = DateTime.Now;
+        Console.WriteLine($"Projeto 7 e 8- Condicionais - {date}\n");
+        int idade = 18;
+        int qtdAdultos = 0;
+        bool taAcompanhado = (qtdAdultos > 0);
+        bool maiorIdade = (idade >= 18);
+        if (maiorIdade || taAcompanhado)
+        {
+            string result = taAcompanhado ? "Tá acompanhado" : "É maior de idade";
+            Console.WriteLine($"Pode passar, {result}");
+        }
+        else
+        {
+            Console.WriteLine("Menor de idade");
+        }
+        Console.WriteLine("Aperte enter para continuar...");
+        Console.ReadLine();
+    }
+}
