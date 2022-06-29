d319f07055b530db8307b84da7da408ed591e4c9
Thiago Kasper de Souza <83460816+ThiagoKS-7@users.noreply.github.com>
2022-06-28 21:38:41 -0300

    Add files via upload

---
+++ 6-Fatorial/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    public static void Fatorial_For_Loop(double num)
+    {
+        Console.WriteLine("\nFATORIAL COM FOR");
+        double i, fat;
+        fat = num;
+        for (i = num - 1; i >= 1; i--)
+        {
+            fat = fat * i;
+
+        }
+        Console.WriteLine($"O fatorial de {num} é {fat}");
+        Console.WriteLine("================ \\\\ ================");
+    }
+    public static void Fatorial_While_Loop(double num)
+    {
+        Console.WriteLine("\nFATORIAL COM WHILE");
+        double fat;
+        fat = num;
+        while ( num != 1 )
+        {
+            fat = fat * num;
+            num--;
+        }
+        Console.WriteLine($"O fatorial de {num} é {fat}");
+        Console.WriteLine("================ \\\\ ================");
+    }
+    static void Main(string[] args)
+    {
+        double num;
+        Console.Write("Informe um número: ");
+        num = double.Parse(Console.ReadLine());
+        Fatorial_For_Loop(num);
+        Fatorial_While_Loop(num);
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadLine();
+    }
+}
